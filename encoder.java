8693492d3d18d9964689c5d7d005ab39280ad08d
alexm388 <alexm@quadprep.org>
2024-11-15 11:18:24 -0500

    Add files via upload

---
+++ encoder.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class encoder {
+ public static void main(String[] args) {
+ Scanner scanner = new Scanner (System .in);
+ System. out. println ("Enter your message: ");
+ String message = scanner.nextLine();
+ System.out.println("Enter the key:" );
+int key = scanner.nextInt();
+ }
+}
+
+
+
